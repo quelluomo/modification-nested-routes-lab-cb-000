f295aa0a2a4a6cd33cdcc59d04ffc8153409e235
Matteo Muscas <thatman.j@gmail.com>
2019-07-01 08:54:08 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -43,8 +43,18 @@ end
   end
 
   def edit
+  if params[:artist_id]
+    artist = Artist.find_by(id: params[:artist_id])
+    if artist.nil?
+      redirect_to artists_path, alert: "artist not found."
+    else
+      @song = artist.songs.find_by(id: params[:id])
+      redirect_to artist_songs_path(artist), alert: "Song not found." if @song.nil?
+    end
+  else
     @song = Song.find(params[:id])
   end
+end
 
   def update
     @song = Song.find(params[:id])
