70f0b3144757341a20d81d71aef95da9a0c083eb
Matteo Muscas <thatman.j@gmail.com>
2019-07-01 09:09:31 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,9 +1,3 @@
 module SongsHelper
-  def artist_select(artist, song)
-      if song.artist.nil?
-        select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
-      else
-        artist.name
-      end
-    end
+
   end
